731849cc23599563fafca1bbe91817dace0ebe73
Scelebi <serdar2225@gmail.com>
2022-03-04 23:32:44 -0500

    first test case

---
+++ cypress/integration/order.js
@@ -0,0 +1,46 @@
+///<reference types="cypress" />
+
+describe("Mercari User Test", () => {
+    before(() => {
+      cy.visit("/");
+    });
+  
+    it("successfully loads the home page", () => {
+      cy.loadHomePage();
+    });
+  
+    it("logs into the site using Env variables", () => {
+      cy.visit("/login");
+  
+      const username = Cypress.env("USERNAME");
+      const password = Cypress.env("PASSWORD");
+      cy.login(username, password);
+  
+      cy.isVisible([
+        "SearchInput",
+        "NotificationsButton",
+        "UserIcon",
+        "ChatIcon",
+      ]);
+    });
+  
+    it("searches for an item", () => {
+      cy.itemSearch();
+    });
+  
+    it("selects item", () => {
+      cy.get("[data-testid=ItemContainer]:first").click();
+    });
+  
+    it("adds item to cart", () => {
+      cy.addToCart();
+    });
+  
+    it("removes item from cart", () => {
+      cy.removeCartItems();
+    });
+  
+    it("logs out", () => {
+      cy.logOut();
+    });
+  });
---
+++ cypress/integration/search/simple_search.js
@@ -0,0 +1,20 @@
+describe('Test Mercari Site using Cypress', ()=>{
+
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('Verify the page', ()=>{
+        cy.get('.Button-y431fn-0 > .Image-sc-172fqpb-1')
+        cy.title().should("eq", "Mercari: Your Marketplace")
+    })
+    it('Search for Shirts', ()=>{
+        cy.get('[data-testid=SearchInput]').type('Shirts')
+        cy.get('[data-testid=SearchIcon] > svg').click().wait(2000)
+        cy.url().should('include', 'Shirts')
+        expect('[data-testid=SearchKeyword]').to.exist
+        cy.contains('Shirts')
+
+    })
+   
+})
